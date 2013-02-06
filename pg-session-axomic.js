61e5c2a2bd54ece7bf352c375b18d81f95fc822f
Patryk Kabaj <patryk.kabaj@axomic.com>
2013-02-06 16:15:28 +0000

    Added globalconfig as a part of options that are passed from Node. It makes it more self-contained.

--- pg-session-axomic.js
+++ pg-session-axomic.js
@@ -1,7 +1,6 @@
-var 	orm					= require('node-orm2-axomic'),
-		winscontrol	     	= require('../../lib/winsControl'),
-		winston 			= winscontrol.Win(),
-		globalConfig        = require('../../globalConfig');
+var     orm                 = require('node-orm2-axomic'),
+        winscontrol         = require('../../lib/winsControl'),
+        winston             = winscontrol.Win();
 
 
 
@@ -10,156 +9,157 @@ module.exports = function (connect)
     function PostgresStore(options)
     {
         options = options || {};
+        var globalConfig = options.globals || {};
         connect.session.Store.call(this, options);
-        
+
         var self = this,
             checkExpirationInterval = options.checkExpirationInterval || 1000*60*10, // default 10 minutes.
             defaultExpiration = options.defaultExpiration || 1000*60*60*24; // default 1 day.
-         
-        
+
+
         var initialized = true;
-        
+
         function initialize(callback)
         {
            callback();
-		}
-        
+        }
+
         // Check periodically to clear out expired sessions.
         setInterval(function ()
         {
-			orm.connect(globalConfig.databaseConnectionPostgres,
-					function(err, db){
-						var Session = db.define("session", {
-							sid:     String,
-							expires: Number,
-							json:    String
-						});
+            orm.connect(globalConfig.databaseConnectionPostgres,
+                    function(err, db){
+                        var Session = db.define("session", {
+                            sid:     String,
+                            expires: Number,
+                            json:    String
+                        });
 
-						
+
                 Session.find({expires: orm.lt(Math.round(Date.now() / 1000))})
-				.remove(function (err) {
-					if(err) {
-					winston.error(err);
-					}
-				})
-					})
+                .remove(function (err) {
+                    if(err) {
+                    winston.error(err);
+                    }
+                });
+                    });
             }, checkExpirationInterval);
-        
-        
+
+
         this.get = function (sid, fn)
         {
-			winston.info("getting session");
-          	orm.connect(globalConfig.databaseConnectionPostgres,
-			function(err, db){
-				var Session = db.define("session", {
-							sid:     String,
-							expires: Number,
-							json:    String
-				});
-				Session.find({sid: sid}, function (err, record)
-                {
-					if(record.length > 0) {
-						var session = JSON.parse(record[0].json);
-						fn(null, session);
-					}
-					else {
-						fn();
-					}
-                })
-                                    
+            winston.info("getting session");
+            orm.connect(globalConfig.databaseConnectionPostgres,
+            function(err, db){
+                var Session = db.define("session", {
+                            sid:     String,
+                            expires: Number,
+                            json:    String
                 });
-  
+                Session.find({sid: sid}, function (err, record)
+                {
+                    if(record.length > 0) {
+                        var session = JSON.parse(record[0].json);
+                        fn(null, session);
+                    }
+                    else {
+                        fn();
+                    }
+                });
+
+                });
+
         };
-        
+
         this.set = function (sid, session, fn)
         {
-			winston.info("setting session");
-			orm.connect(globalConfig.databaseConnectionPostgres,
-			function(err, db){
-				var Session = db.define("session", {
-							sid:     String,
-							expires: Number,
-							json:    String,
-				});
+            winston.info("setting session");
+            orm.connect(globalConfig.databaseConnectionPostgres,
+            function(err, db){
+                var Session = db.define("session", {
+                            sid:     String,
+                            expires: Number,
+                            json:    String
+                });
 
                 Session.find({sid: sid}, function (err, recordArray)
                 {
-					var record;
-					var expires = session.cookie.expires ||
-					  new Date(Date.now() + defaultExpiration);
-					var expirytime =  Math.round(expires.getTime() / 1000);
+                    var record;
+                    var expires = session.cookie.expires ||
+                      new Date(Date.now() + defaultExpiration);
+                    var expirytime =  Math.round(expires.getTime() / 1000);
 
-                    if (recordArray.length == 0)
+                    if (recordArray.length === 0)
                     {
-						record = new Session({sid: sid,
-						json: JSON.stringify(session),
-						expires: expirytime });
+                        record = new Session({sid: sid,
+                        json: JSON.stringify(session),
+                        expires: expirytime });
 
-						record.save(function(err) {
-							if(err) {
-							   winston.info("error is");
-							   winston.info(err);
-							}
-						});
-					}
-					else {
-						recordArray[0].json = JSON.stringify(session);
-						recordArray[0].expires = expirytime;
-						recordArray[0].save(function(err) {
-							if(err) {
-								   winston.error(err);
-								}
-						});
+                        record.save(function(err) {
+                            if(err) {
+                               winston.info("error is");
+                               winston.info(err);
+                            }
+                        });
+                    }
+                    else {
+                        recordArray[0].json = JSON.stringify(session);
+                        recordArray[0].expires = expirytime;
+                        recordArray[0].save(function(err) {
+                            if(err) {
+                                   winston.error(err);
+                                }
+                        });
 
-					}
-                  	fn();
-                })
-			 })
-		};
-        
+                    }
+                    fn();
+                });
+             });
+        };
+
         this.destroy = function (sid, fn)
         {
                 orm.connect(globalConfig.databaseConnectionPostgres,
-			function(err, db){
-				var Session = db.define("session", {
-							sid:     String,
-							expires: Number,
-							json:    String,
-				});
+            function(err, db){
+                var Session = db.define("session", {
+                            sid:     String,
+                            expires: Number,
+                            json:    String
+                });
 
                 Session.find({sid: sid}).remove(function (err) {
-					winston.error(err);
+                    winston.error(err);
                     fn && fn();
-				})
+                });
 
-			});
-		};
+            });
+        };
 
 
         this.length = function (callback)
         {
            orm.connect(globalConfig.databaseConnectionPostgres,
-			function(err, db){
-				var Session = db.define("session", {
-							sid:     String,
-							expires: Number,
-							json:    String,
-				});
+            function(err, db){
+                var Session = db.define("session", {
+                            sid:     String,
+                            expires: Number,
+                            json:    String
+                });
 
 
           Session.count({}, function (err, count){
-                callback(count)
-       		 });
-			})
-		}
+                callback(count);
+             });
+            });
+        };
         this.clear = function (callback)
         {
-			winston.info("Clear ALL SESSIONS!");
+            winston.info("Clear ALL SESSIONS!");
         };
 
     }
-    
+
     PostgresStore.prototype.__proto__ = connect.session.Store.prototype;
-    
+
     return PostgresStore;
 };
